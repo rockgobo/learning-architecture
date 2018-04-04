ce6bc53b6bf63416e82516a95e2356a7449d06e0
rockgobo <christian.matyas@gmail.com>
2018-04-04 15:02:13 +0200

    Erste Aufteilung in Aufgaben

--- GoF_Strategie_Taschenrechner/GoF_Strategie_Taschenrechner/Program.cs
+++ GoF_Strategie_Taschenrechner/GoF_Strategie_Taschenrechner/Program.cs
@@ -6,17 +6,68 @@ namespace GoF_Strategie_Taschenrechner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Formel eingeben:");
-            string eingabe = Console.ReadLine(); // "2 + 2"
 
-            string[] operanden = eingabe.Split('+');
-            int zahl1 = Convert.ToInt32(operanden[0]);
-            int zahl2 = Convert.ToInt32(operanden[1]);
-
-            Console.WriteLine($"Das Ergebnis is {zahl1 + zahl2}");
+            new ConsolenUI().Start();
 
             Console.WriteLine("---Ende---");
             Console.ReadKey();
+        }
+    }
+
+    class Formel
+    {
+        public int Operand1 { get; set; }
+        public int Operand2 { get; set; }
+        public char Operator { get; set; }
+    }
+
+    class Parser
+    {
+        public Formel Parse(String input) {
+            Formel output = new Formel();
+            string[] operanden = input.Split('+'); //TODO: regex
+
+            output.Operand1 = Convert.ToInt32(operanden[0]);
+            output.Operand2 = Convert.ToInt32(operanden[1]);
+            output.Operator = '+';
+
+            return output;
+        }
+    }
+
+    class Rechner
+    {
+        public int Rechne(Formel formel)
+        {
+            if(formel.Operator == '+')
+            {
+                return formel.Operand1 + formel.Operand2;
+            }
+            else if (formel.Operator == '-')
+            {
+                return formel.Operand1 - formel.Operand2;
+            }
+            else
+            {
+                throw new ArgumentException("Operator unbekannt");
+            }
+        }
+    }
+
+    class ConsolenUI{
+        public void Start()
+        {
+            Console.WriteLine("Formel eingeben:");
+            string eingabe = Console.ReadLine(); // "2 + 2"
+
+            Parser parser = new Parser();
+            Rechner rechner = new Rechner();
+
+            Formel x = parser.Parse(eingabe);
+            var ergebnis = rechner.Rechne(x);
+
+            Console.WriteLine($"Das Ergebnis is {ergebnis}");
+
         }
     }
 }
