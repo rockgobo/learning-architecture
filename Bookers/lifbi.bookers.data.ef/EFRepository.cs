3d390d27fd78d0836c7460b470cc6726373eb680
rockgobo <christian.matyas@gmail.com>
2018-04-05 13:49:06 +0200

    EFRepository implementiert

---
+++ Bookers/lifbi.bookers.data.ef/EFRepository.cs
@@ -0,0 +1,44 @@
+﻿using lifbi.bookers.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lifbi.bookers.data.ef
+{
+    class EFRepository : IRepository
+    {
+        public EFRepository(EFContext context) => this.context = context;
+        private readonly EFContext context;
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetByID<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetByID<T>(entity.ID);
+
+            if(loaded != null)
+            {
+                context.Entry(loaded).CurrentValues.SetValues(entity);
+            }
+        }
+    }
+}
