d4e24785ce0bb341ebfc04045f069ee7f7c922c8
rockgobo <christian.matyas@gmail.com>
2018-04-05 14:23:36 +0200

    Simple Ausgabe von allen Buchläden und deren Bücher

--- Bookers/lifbi.bookers.ui.console/Program.cs
+++ Bookers/lifbi.bookers.ui.console/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using lifbi.bookers.logic;
+using lifbi.bookers.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,20 @@ namespace lifbi.bookers.ui.console
     {
         static void Main(string[] args)
         {
+            Core core = new Core();
 
+            if (core.Repository.GetAll<BookShop>().Count() == 0)
+                core.GenerateDemoData();
+
+            foreach (var shop in core.Repository.GetAll<BookShop>())
+            {
+                Console.WriteLine($"[{shop.Name}] - Standort: {shop.Address}");
+                foreach (var stock in shop.Inventory)
+                    Console.WriteLine($"[{shop.Name}] - Buch: {stock.Book.Title} - Anzahl: {stock.Amount}");
+            }
+
+            Console.WriteLine("--- Ende ---");
+            Console.ReadKey();
         }
     }
 }
