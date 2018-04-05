1bd3f8271bc94c388082969b39aac7d357928367
rockgobo <christian.matyas@gmail.com>
2018-04-05 10:09:31 +0200

    init model

--- GoF_Strategie_Taschenrechner/GoF_Strategie_Taschenrechner/Program.cs
+++ GoF_Strategie_Taschenrechner/GoF_Strategie_Taschenrechner/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace GoF_Strategie_Taschenrechner
+namespace GoF_Strategie_Taschenrechnerl
 {
     class Program
     {
