14d8af14163bb3c10dcbea07e016009a2756d92b
rockgobo <christian.matyas@gmail.com>
2018-04-05 13:59:14 +0200

    implementiert Konstruktor mit Dependency Injection

--- Bookers/lifbi.bookers.logic/Core.cs
+++ Bookers/lifbi.bookers.logic/Core.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using lifbi.bookers.model;
+using lifbi.bookers.data.ef;
+using System;
 
 namespace lifbi.bookers.logic
 {
     public class Core
     {
+        public IRepository Repository { get; set; }
+
+        public Core(IRepository repository) => this.Repository = repository;
+        public Core() : this(new EFRepository(new EFContext())) { }
     }
 }
