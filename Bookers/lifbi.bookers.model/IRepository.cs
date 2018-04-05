0781e03c28e45b4cf2a20b5bcc22053783b472f8
rockgobo <christian.matyas@gmail.com>
2018-04-05 13:29:43 +0200

    Interface für Repository erstellt.

---
+++ Bookers/lifbi.bookers.model/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lifbi.bookers.model
+{
+    public interface IRepository
+    {
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        T GetByID<T>(int id) where T : Entity;
+        void Add<T>(T entity) where T : Entity;
+        void Update<T>(T entity) where T : Entity;
+        void Save();
+    }
+}
