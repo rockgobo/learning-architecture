8813893887ca9f2ab52e6ca5cab2c6ad58703368
rockgobo <christian.matyas@gmail.com>
2018-04-05 16:29:30 +0200

    WPF UI

---
+++ Bookers/lifbi.bookers.ui.wpf/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using lifbi.bookers.logic;
+using lifbi.bookers.model;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lifbi.bookers.ui.wpf
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            core = new Core();
+
+            books = core.Repository.GetAll<Book>();
+        }
+        private readonly Core core;
+
+        private IEnumerable<Book> books;
+        public IEnumerable<Book> Books {
+            get => books;
+            set => SetProperty(ref books, value);
+        }
+    }
+}
